32f30d84275facf08f9d956e7ca0a3cb20f0773a
vkhudify <vkhudify@gmail.com>
2024-11-08 16:02:57 +0300

    Added console command to make first user in database as developer

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('setDeveloper', function () {
+    $user = User::first();
+    if ($user) {
+        $user->isDeveloper = 1;
+        $user->save();
+        $this->info('First user is now a developer!');
+    } else {
+        $this->info('No users found in the database.');
+    }
+});
