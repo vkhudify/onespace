0ca4a4b41deb44782199ef4f321a8deb6b3b2104
vkhudify <vkhudify@gmail.com>
2024-11-08 15:15:58 +0300

    Added auth middleware for group of testing routes

--- routes/testingzone.php
+++ routes/testingzone.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TestingZone\TestingZoneController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/testing')->middleware('dev')->group(function ()
+Route::prefix('/testing')->middleware(['dev', 'auth'])->group(function ()
 {
-    Route::get('/', [TestingZoneController::class, 'welcome'])->name('testing.welcome')->middleware('dev');
+    Route::get('/', [TestingZoneController::class, 'welcome'])->name('testing.welcome');
 });
